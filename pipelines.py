f8a5644641fe2651438e0a780d98c214a8204a63
Thiago Pereira <72038599+Thiago2VP@users.noreply.github.com>
2024-01-05 20:22:07 -0300

    pipeline iniciado

---
+++ pipelines.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+
+os.system("echo hello world")
+print("Parte 1")
+# variavel = subprocess.call(["py", "list-operations.py"])
+variavel = subprocess.check_output(["py", "list-operations.py"])
+print("Parte 2")
+print(variavel)
