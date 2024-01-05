7bf32b1d91a62dff18aa57109b11350294fbec36
Thiago Pereira <72038599+Thiago2VP@users.noreply.github.com>
2024-01-05 16:09:57 -0300

    estudo de caso adicionado

---
+++ visualizar.py
@@ -0,0 +1,27 @@
+entrada = open("rna-escherichia.fasta").read()
+saida = open("bacteria.html", "w")
+# entrada = open("rna-human.fasta").read()
+# saida = open("humano.html", "w")
+
+cont = {}
+
+for i in ['A', 'T', 'C', 'G']:
+    for j in ['A', 'T', 'C', 'G']:
+        cont[i+j] = 0
+
+entrada = entrada.replace("\n", "")
+
+for k in range(len(entrada)-1):
+    cont[entrada[k]+entrada[k+1]] += 1
+
+# html
+i = 1
+for k in cont:
+    transparencia = cont[k]/max(cont.values())
+    saida.write("<div style='width:100px; border: 1px solid #111; height:100px; float: left; background-color: rgba("
+                "0, 0, 0, " + str(transparencia) + "); color: #fff'>" + k + "</div>")
+    if i % 4 == 0:
+        saida.write("<div style='clear:both'></div>")
+    i += 1
+
+saida.close()
