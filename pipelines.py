5602bea45a609286bffcad692d8340d27bfc2839
Thiago Pereira <72038599+Thiago2VP@users.noreply.github.com>
2024-01-05 20:48:40 -0300

    mudança para rodar no linux

--- pipelines.py
+++ pipelines.py
@@ -4,6 +4,7 @@ import subprocess
 os.system("echo hello world")
 print("Parte 1")
 # variavel = subprocess.call(["py", "list-operations.py"])
-variavel = subprocess.check_output(["py", "list-operations.py"])
+# variavel = subprocess.check_output(["py", "list-operations.py"])
+variavel = subprocess.check_output(["python3", "list-operations.py"])
 print("Parte 2")
 print(variavel)
